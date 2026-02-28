a1f9564157893aeaa52c99a1e41de2b321179af1
Aniketdhar0810 <https://github.com/Aniketdhar810/Animan>
2026-02-28 12:20:04 +0530

    changes

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 const roomController = require("../controllers/roomController");
+const messageController = require("../controllers/messageController");
 
 const requireAuth = passport.authenticate("jwt", { session: false });
 
@@ -9,5 +10,6 @@ router.get("/mine", requireAuth, roomController.listMyRooms);
 router.get("/:roomId", requireAuth, roomController.getRoomById);
 router.post("/join", requireAuth, roomController.joinRoomByCode);
 router.post("/:roomId/leave", requireAuth, roomController.leaveRoom);
+router.get("/:roomId/messages", requireAuth, messageController.getMessages);
 
 module.exports = router;
